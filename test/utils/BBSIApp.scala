cdf9595c0ddfe07298a31d2b6632c1490c4d7203
KrishnaBorra <krishna.borra@digital.hmrc.gov.uk>
2018-04-16 17:34:03 +0100

    BBSI-56: Migration of BBSI Overview page and its dependancies

---
+++ test/utils/BBSIApp.scala
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils
+
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.scalatest.TestSuite
+import org.scalatest.concurrent.PatienceConfiguration
+import org.scalatestplus.play.OneServerPerSuite
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+
+trait BBSIApp extends OneServerPerSuite with PatienceConfiguration {
+  this: TestSuite =>
+
+  implicit override lazy val app: Application = new GuiceApplicationBuilder().build()
+}
+
+
