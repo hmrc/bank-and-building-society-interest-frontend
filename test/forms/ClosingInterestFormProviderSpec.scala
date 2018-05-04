bce2dbbe797714f3ab5ab32113e0aaa7bdf5a9cc
John Lewis <john.lewis@digital.hmrc.gov.uk>
2018-05-04 17:08:09 +0100

    BBSI-13:Added from spec

--- test/forms/ClosingInterestFormProviderSpec.scala
+++ test/forms/ClosingInterestFormProviderSpec.scala
@@ -16,38 +16,97 @@
 
 package forms
 
-import forms.behaviours.BooleanFieldBehaviours
-import play.api.data.FormError
+import base.SpecBase
+import play.api.i18n.{I18nSupport, Messages}
+import play.api.libs.json.Json
+import utils.FormValuesConstants
 
-class ClosingInterestFormProviderSpec extends BooleanFieldBehaviours {
-
-  val requiredKey = "closingInterest.error.required"
-  val invalidKey = "error.boolean"
+class ClosingInterestFormProviderSpec extends SpecBase with FormValuesConstants with I18nSupport{
 
   val form = new ClosingInterestFormProvider()()
 
   "closingBankAccountInterestChoice" must {
 
-    val fieldName = "value"
+    "return no errors" when {
+      "given a valid 'yes' choice and interest amount" in {
+        val validYesChoice = Json.obj(choice -> YesValue, interest -> "123")
+        val validatedForm = form.bind(validYesChoice)
 
-//    behave like booleanField(
-//      form,
-//      fieldName,
-//      invalidError = FormError(fieldName, invalidKey)
-//    )
+        validatedForm.errors mustBe empty
+        validatedForm.value mustBe Some(BankAccountClosingInterestForm(Some(YesValue), Some("123")))
+      }
+    }
 
-//    behave like mandatoryField(
-//      form,
-//      fieldName,
-//      requiredError = FormError(fieldName, requiredKey)
-//    )
+    "given a valid 'no' choice and no interest amount" in {
+      val validNoChoice = Json.obj(choice -> NoValue, interest -> "")
+      val validatedForm = form.bind(validNoChoice)
+
+      validatedForm.errors mustBe empty
+      validatedForm.value mustBe Some(BankAccountClosingInterestForm(Some(NoValue), None))
+    }
+
+    "given a valid 'no' choice and interest amount as space" in {
+      val validNoChoice = Json.obj(choice -> NoValue, interest -> " ")
+      val validatedForm = form.bind(validNoChoice)
+
+      validatedForm.errors mustBe empty
+      validatedForm.value mustBe Some(BankAccountClosingInterestForm(Some(NoValue), None))
+    }
+
+    "given a valid 'no' choice and interest amount" in {
+      val validNoChoice = Json.obj(choice -> NoValue, interest -> "123.45")
+      val validatedForm = form.bind(validNoChoice)
+
+      validatedForm.errors mustBe empty
+      validatedForm.value mustBe Some(BankAccountClosingInterestForm(Some(NoValue), None))
+    }
+
+    "return an error" when {
+
+      "given an invalid choice" in {
+        val invalidChoice = Json.obj(choice -> "", interest -> "")
+        val invalidatedForm = form.bind(invalidChoice)
+
+        invalidatedForm.errors.head.messages mustBe List("closingInterest.error.selectOption")
+        invalidatedForm.value mustBe None
+      }
+
+      "given an invalid choice with empty values" in {
+        val invalidChoice = Json.obj(interest -> "")
+        val invalidatedForm = form.bind(invalidChoice)
+
+        invalidatedForm.errors.head.messages mustBe List("closingInterest.error.selectOption")
+        invalidatedForm.value mustBe None
+      }
+
+      "given a valid 'yes' choice but no interest amount" in {
+        val invalidYesChoice = Json.obj(choice -> Some(YesValue), interest -> "")
+        val invalidatedForm = form.bind(invalidYesChoice)
+
+        invalidatedForm.errors.head.messages mustBe List("closingInterest.error.blank")
+        invalidatedForm.value mustBe None
+      }
+
+      "given a valid 'yes' choice but a non numeric interest amount" in {
+        val invalidYesChoice = Json.obj(choice -> Some(YesValue), interest -> "abc")
+        val invalidatedForm = form.bind(invalidYesChoice)
+
+        invalidatedForm.errors.head.messages mustBe List("update.form.interest.isCurrency")
+        invalidatedForm.value mustBe None
+      }
+
+      "given a valid 'yes' choice but decimal interest amount" in {
+        val invalidYesChoice = Json.obj(choice -> Some(YesValue), interest -> "123.45")
+        val invalidatedForm = form.bind(invalidYesChoice)
+
+        invalidatedForm.errors.head.messages mustBe List("update.form.interest.wholeNumber")
+        invalidatedForm.value mustBe None
+      }
+    }
+
   }
 
-
-  "closingInterestEntry" must {
-
-  }
-
-
+  val choice = BankAccountClosingInterestForm.ClosingInterestChoice
+  val interest = BankAccountClosingInterestForm.ClosingInterestEntry
 
 }
