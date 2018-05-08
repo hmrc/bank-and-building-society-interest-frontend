b71f3ff7395db3a65703619ca5bc5605938fc0e0
KrishnaBorra <krishna.borra@digital.hmrc.gov.uk>
2018-05-08 16:53:50 +0100

    BBSI-13-pt2: Check your answers view spec amended to include more tests

--- test/views/CloseCheckAnswersViewSpec.scala
+++ test/views/CloseCheckAnswersViewSpec.scala
@@ -30,15 +30,19 @@ class CloseCheckAnswersViewSpec extends ViewBehaviours {
 
   val id = 1
   val currentTaxYear = TaxYear().year
-  val closeBankAccountDate = new LocalDate(currentTaxYear, 4, 4)
+  val closeBankAccountDate = new LocalDate(currentTaxYear, 5, 4)
+  val closeBankAccountDate1 = new LocalDate(currentTaxYear - 1, 5, 4)
+  val displayAmount = "4,000"
   val closeBankAccountName = "TEST NAME"
   val interestAmount = "4000"
 
   val viewModel = CloseBankAccountCheckAnswersViewModel(id, closeBankAccountDate.toString(), Some(closeBankAccountName), Some(interestAmount))
+  val viewModel1 = CloseBankAccountCheckAnswersViewModel(id, closeBankAccountDate1.toString(), Some(closeBankAccountName), None)
 
   def createView = () => close_account_check_your_answers(frontendAppConfig, viewModel)(fakeRequest, messages, templateRenderer)
+  def createView1 = () => close_account_check_your_answers(frontendAppConfig, viewModel1)(fakeRequest, messages, templateRenderer)
 
-  "CheckYourAnswers view when interest amount is not provided" must {
+  "CheckYourAnswers view when interest amount is provided" must {
     behave like normalPage(createView,
       messageKeyPrefix,
       Some(messages("checkYourAnswers")),
@@ -53,16 +57,47 @@ class CloseCheckAnswersViewSpec extends ViewBehaviours {
     "display change links for close account journey" when {
       behave like pageWithChangeLink(createView, 1, controllers.routes.DecisionController.onPageLoad(NormalMode, id).url + "?edit=true")
       behave like pageWithChangeLink(createView, 2, controllers.routes.CloseAccountController.onPageLoad(CheckMode).url + "?edit=true")
+      behave like pageWithChangeLink(createView, 3, controllers.routes.ClosingInterestController.onPageLoad(CheckMode).url + "?edit=true")
     }
 
     "display question lines for close account journey" when {
       behave like pageWithQuestionLine(createView, 1, messages("checkYourAnswers.whatYouToldUs"))
       behave like pageWithQuestionLine(createView, 2, messages("close.checkYourAnswers.rowTwo.question"))
+      behave like pageWithQuestionLine(createView, 3, messages("close.checkYourAnswers.rowThree.question", currentTaxYear.toString))
     }
 
     "display Answer lines for close account journey" when {
       behave like pageWithAnswerLine(createView, 1, messages("close.checkYourAnswers.rowOne.answer"))
       behave like pageWithAnswerLine(createView, 2, Dates.formatDate(new LocalDate(viewModel.closeBankAccountDate)))
+      behave like pageWithAnswerLine(createView, 3, "£" + displayAmount)
+    }
+  }
+
+  "CheckYourAnswers view when interest amount is not provided" must {
+    behave like normalPage(createView1,
+      messageKeyPrefix,
+      Some(messages("checkYourAnswers")),
+      Some(messages("checkYourAnswers")),
+      Some(messages("closeAccount.preHeading")))
+    behave like pageWithSubmitButton(createView1, controllers.routes.CheckYourAnswersController.onSubmitClose().url, messages("confirmAndSend"))
+    behave like pageWithBackLink(createView1)
+    behave like pageWithCancelLink(createView1)
+    behave like pageWithText(createView1, messages("checkYourAnswers.confirmText"))
+    behave like pageWithHeadingH2(createView1, viewModel1.closeBankAccountName.getOrElse(""))
+
+    "display change links for close account journey" when {
+      behave like pageWithChangeLink(createView1, 1, controllers.routes.DecisionController.onPageLoad(NormalMode, id).url + "?edit=true")
+      behave like pageWithChangeLink(createView1, 2, controllers.routes.CloseAccountController.onPageLoad(CheckMode).url + "?edit=true")
+    }
+
+    "display question lines for close account journey" when {
+      behave like pageWithQuestionLine(createView1, 1, messages("checkYourAnswers.whatYouToldUs"))
+      behave like pageWithQuestionLine(createView1, 2, messages("close.checkYourAnswers.rowTwo.question"))
+    }
+
+    "display Answer lines for close account journey" when {
+      behave like pageWithAnswerLine(createView1, 1, messages("close.checkYourAnswers.rowOne.answer"))
+      behave like pageWithAnswerLine(createView1, 2, Dates.formatDate(new LocalDate(viewModel1.closeBankAccountDate)))
     }
   }
 }
