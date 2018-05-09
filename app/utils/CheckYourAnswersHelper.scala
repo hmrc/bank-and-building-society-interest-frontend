9ab6702fe276006049f11eb26dc4a8a9a521d1ba
John Lewis <john.lewis@digital.hmrc.gov.uk>
2018-05-09 12:56:11 +0100

    BBSI-13:Replaced x values with meaningful names

--- app/utils/CheckYourAnswersHelper.scala
+++ app/utils/CheckYourAnswersHelper.scala
@@ -22,19 +22,22 @@ import viewmodels.AnswerRow
 
 class CheckYourAnswersHelper(userAnswers: UserAnswers) {
 
-  def updateInterest: Option[AnswerRow] = userAnswers.updateInterest map {
-    x => AnswerRow("updateInterest.checkYourAnswersLabel", s"$x", false, routes.UpdateInterestController.onPageLoad(CheckMode).url)
+  def updateInterest: Option[AnswerRow] = userAnswers.updateInterest map { interestAmount =>
+    AnswerRow("updateInterest.checkYourAnswersLabel", s"$interestAmount", false, routes.UpdateInterestController.onPageLoad(CheckMode).url)
   }
 
-  def closingInterest: Option[AnswerRow] = userAnswers.closingInterest map {
-    x => AnswerRow("closingInterest.checkYourAnswersLabel", s"${x.closingBankAccountInterestChoice} ${x.closingInterestEntry}", true, routes.ClosingInterestController.onPageLoad(CheckMode).url)
+  def closingInterest: Option[AnswerRow] = userAnswers.closingInterest map { bankAccountClosingInterestForm =>
+    AnswerRow("closingInterest.checkYourAnswersLabel",
+      s"${bankAccountClosingInterestForm.closingBankAccountInterestChoice} ${bankAccountClosingInterestForm.closingInterestEntry}",
+      true, routes.ClosingInterestController.onPageLoad(CheckMode).url)
   }
 
-  def closeAccount: Option[AnswerRow] = userAnswers.closeAccount map {
-    x => AnswerRow("closeAccount.checkYourAnswersLabel", s"${x.accountClosedDay} ${x.accountClosedMonth} ${x.accountClosedYear}", false, routes.CloseAccountController.onPageLoad(CheckMode).url)
+  def closeAccount: Option[AnswerRow] = userAnswers.closeAccount map { accountClose =>
+    AnswerRow("closeAccount.checkYourAnswersLabel", s"${accountClose.accountClosedDay} ${accountClose.accountClosedMonth} ${accountClose.accountClosedYear}",
+      false, routes.CloseAccountController.onPageLoad(CheckMode).url)
   }
 
-  def decision: Option[AnswerRow] = userAnswers.decision map {
-    x => AnswerRow("decision.checkYourAnswersLabel", s"decision.$x", true, routes.DecisionController.onPageLoad(CheckMode, 0).url)
+  def decision: Option[AnswerRow] = userAnswers.decision map { choice =>
+    AnswerRow("decision.checkYourAnswersLabel", s"decision.$choice", true, routes.DecisionController.onPageLoad(CheckMode, 0).url)
   }
 }
